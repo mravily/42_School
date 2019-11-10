27324a8f87c8e42566884b5efa49d2c5a3aa2f49
Med_Wo <medhi.filahi@hotmail.com>
2019-11-10 14:12:24 +0100

    maj strnstr

--- Student/Libft/ft_strnstr.c
+++ Student/Libft/ft_strnstr.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_haystacknhaystack.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mravily <mravily@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -13,25 +13,22 @@
 #include "libft.h"
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 {
-	size_t		i;
-	size_t		j;
+	size_t i;
+	size_t j;
 
 	i = 0;
 	j = 0;
-		if (needle[j] == '\0')
+	if (needle[0] == '\0')
 		return ((char *)haystack);
-	while (haystack[i] && needle[j] && i < n)
+	while (haystack[i] && i < n)
 	{
-		while (needle[j] == haystack[i])
-		{
-			if (haystack[i] != needle[j])
-				j = 0;
+		if (haystack[i] == needle[j])
 			j++;
-		}
+		else
+			j = 0;
 		i++;
+		if (needle[j] == '\0')
+			return ((char *)haystack + (i - j));
 	}
-	if (needle[j] == '\0')
-		return ((char*)haystack + i + 1 - j);
-	else
-		return (NULL);
+	return (0);
 }
