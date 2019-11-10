eca28e68465a3cbcbf50358d3e8f16023ebd35fb
Med_Wo <medhi.filahi@hotmail.com>
2019-11-10 17:43:22 +0100

    maj ft_strjoin + main

--- Student/Libft/ft_strjoin.c
+++ Student/Libft/ft_strjoin.c
@@ -53,21 +53,3 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	join[i] = '\0';
 	return (join);
 }
-
-#include <stdio.h>
-int		main(void)
-{
-	char	*s1 = "where is my ";
-	char	*s2 = "malloc ???";
-	char	*s3 = "where is my malloc ???";
-
-	char *res = ft_strjoin(s1, s2);
-	if (res == s3)
-		printf("TEST_SUCCESS\n");
-	else
-		printf("TEST_FAILED\n");
-
-	puts("Diff :\n");
-	printf("strjoin = %s\n", s3);
-	printf("ft_strjoin = %s\n", ft_strjoin(s1, s2));
-}
--- Student/Main Libft/main_strjoin.c
+++ Student/Main Libft/main_strjoin.c
@@ -6,8 +6,12 @@ int		main(void)
 	char	*s3 = "where is my malloc ???";
 
 	char *res = ft_strjoin(s1, s2);
-
 	if (res == s3)
-		printf("TEST_SUCCESS");
+		printf("TEST_SUCCESS\n");
 	else
-		printf("TEST_FAILED");
+		printf("TEST_FAILED\n");
+
+	puts("Diff :\n");
+	printf("strjoin = %s\n", s3);
+	printf("ft_strjoin = %s\n", ft_strjoin(s1, s2));
+}
