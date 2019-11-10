6672edeb23b4194c1ba5423a82039254e653da53
Med_Wo <medhi.filahi@hotmail.com>
2019-11-10 11:35:40 +0100

    maj substr

--- Student/Libft/ft_substr.c
+++ Student/Libft/ft_substr.c
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mravily <mravily@student.42.fr>            +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 20:46:55 by mravily           #+#    #+#             */
-/*   Updated: 2019/11/04 21:30:32 by mravily          ###   ########.fr       */
+/*   Updated: 2019/11/10 11:35:22 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,37 +30,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub[j] = '\0';
 	return (sub);
 }
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mravily <mravily@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/04 20:46:55 by mravily           #+#    #+#             */
-/*   Updated: 2019/11/04 21:30:32 by mravily          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	unsigned char	*sub;
-	unsigned int	j;
-
-	j = 0;
-	sub = NULL;
-	if (!(sub = (char *)malloc(sizeof(char) * len + 1)))
-		return (NULL);
-	while (start < len)
-	{
-		sub[j] = s[start];
-		start++;
-		j++;
-	}
-	sub[j] = '\0';
-	return (sub);
-}
->>>>>>> 2f0b94d96dbebde8e0c96cfe162370bc48ff3ee1
