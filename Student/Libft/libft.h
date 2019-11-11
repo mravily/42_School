405034d8df1e866044f9c589e4ee9e119eeb9c05
Med_Wo <medhi.filahi@hotmail.com>
2019-11-11 11:36:53 +0100

    maj libft.f, makefile && add strncmp

--- Student/Libft/libft.h
+++ Student/Libft/libft.h
@@ -29,6 +29,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 // ft_strlcpy //
 size_t	ft_strlcat(char *dest, const char *src, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n);               
