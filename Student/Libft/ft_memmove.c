606d584ac66906768ae1dccbc1da95eb4573347b
Med_Wo <medhi.filahi@hotmail.com>
2019-11-11 10:32:10 +0100

    maj memmove

--- Student/Libft/ft_memmove.c
+++ Student/Libft/ft_memmove.c
@@ -14,11 +14,14 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	unsigned char	tmp[n];
+	unsigned char	*tmp;
 
 	if (!dst || !src)
 		return (NULL);
+	if (!(tmp = (unsigned char *)malloc(sizeof(char) * n)))
+		return (NULL);
 	ft_memcpy(tmp, src, n);
 	ft_memcpy(dst, tmp, n);
+	free (tmp);
 	return (dst);
 }
